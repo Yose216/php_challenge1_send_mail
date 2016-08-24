9531cc9941f377c2391e6d568bf880877ef841f7
Yose <youness;chetoui@gmail.com>
2016-08-24 16:45:55 +0200

    First commit

---
+++ mail.php
@@ -0,0 +1,9 @@
+<form method="post">
+            <label>Adresse : </label> 
+				<input type="mail" name="adresse" placeholder="xx@example.com"/>
+            <label>objet : </label> 
+				<input type="texte" name="objet" placeholder="objet"/>
+            <label>texte : </label> 
+				<textarea type="text" name="msg" maxlength="1000" placeholder="Votre message!"></textarea>
+            <input type="submit" value ="Valider"/>
+        </form>
