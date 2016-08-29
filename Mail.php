9567341ebb04447e84fb17ffadea8d9ff4aaf8d8
Yose <youness;chetoui@gmail.com>
2016-08-29 15:03:34 +0200

    debug envoi avec espace

--- Mail.php
+++ Mail.php
@@ -1,28 +1,28 @@
 <?php 
-    $to = $_POST['adresse'];
-    $headers = 'From: Youness.chetoui@face.com' . "\r\n" .
+    $mail = trim($_POST['adresse']);
+    $header = 'From: Youness.chetoui@face.com' . "\r\n" .
                 'Reply-To: no-reply@example.com' . "\r\n" .
                 'X-Mailer: PHP/' . phpversion();
-    $subject = $_POST['objet'];
-    $message = $_POST['msg'];
+    $objet = trim($_POST['objet']);
+    $message = trim($_POST['msg']);
 
 	if (isset($_POST['envoye']))
 	{
-    	if (empty($to) || empty($headers) || empty($subject) || empty($message)) //verifie la presence de contenue
+    	if (empty($mail) || empty($header) || empty($objet) || empty($message)) //verifie si il y a quelque chose dans les input
 		{  
     		echo '<script>alert("Tout les champs doivent être renseignés")</script><br/>
-                  <a href="http://localhost/php_challenge1_send_mail/index.php">Retour au mail</a>';
+                  <a href="http://localhost/php_challenge1_send_mail/index.php">Retour au formulaire</a>';
 		} 
- 		else if (mail($to, $subject, $message, $headers)) //envoie le mail et met en place un cookie
+ 		else if (mail($mail, $objet, $message, $header)) //envoie le mail et met en place un cookie
         {
             $alert = '<script>alert("E-mail envoyé avec succès")</script><br/>
-                      <a href="http://localhost/php_challenge1_send_mail/index.php">Retour au mail</a>';
+                      <a href="http://localhost/php_challenge1_send_mail/index.php">Retour au formulaire</a>';
             setcookie("sent", "1", time() + 120);
         }
-        else
+        else //affiche un message d'erreur si sa ne s'envoi pas
         {
             $alert = '<script>alert("Erreur d\'envoi de l\'e-mail")</script><br/>
-                      <a href="http://localhost/php_challenge1_send_mail/index.php">Retour au mail</a>';
+                      <a href="http://localhost/php_challenge1_send_mail/index.php">Retour au formulaire</a>';
         }
         
         echo $alert;
