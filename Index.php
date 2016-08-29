9bbb138c5b41b756d96fdc941e631859b87bc8e8
Yose <youness;chetoui@gmail.com>
2016-08-29 11:00:21 +0200

    rajout verif contenue dans les input

--- Index.php
+++ Index.php
@@ -11,16 +11,15 @@
         <body>
             <h1 class="col-lg-offset-4">Formulaire d'envoi de mail</h1>
             
-                <br/><form method="post" class="col-lg-offset-3">
+                <br/><form method="post" action="Mail.php" class="col-lg-offset-3">
                         <label>E-mail : </label> 
 				            <input type="email" name="adresse" placeholder="example@example.com"/><br/>
                     <br/><label>Objet : </label> 
 				            <input type="texte" name="objet" placeholder="Ex : Travail"/><br/>
                     <br/><label>Texte : </label><br/> 
 				            <textarea type="text" name="msg" maxlength="500" placeholder="Votre message!" rows="8" cols="60"></textarea><br/>
-                    <br/><button class="btn btn-success col-lg-2 col-lg-offset-2" type="submit" value ="Valider">Envoi</button>
+                    <br/><button class="btn btn-success col-lg-2 col-lg-offset-2" type="submit" value ="Valider" name='envoye'>Envoi</button>
                     </form>
-            <?php include("Mail.php") ?>
             
         </body>
     </html>
--- Mail.php
+++ Mail.php
@@ -5,5 +5,23 @@
                 'X-Mailer: PHP/' . phpversion();
     $subject = $_POST['objet'];
     $message = $_POST['msg'];
-    mail($to, $subject, $message, $headers);
+
+	if (isset($_POST['envoye']))
+	{
+    	if (empty($to) || empty($headers) || empty($subject) || empty($message))
+		{  
+    		echo '<strong> les champs doivent être renseignés</strong>';
+		} 
+ 		else if (mail($to, $subject, $message, $headers))
+        {
+            $alert = '<strong>E-mail envoyé avec succès</strong>';
+            setcookie("sent", "1", time() + 120);
+        }
+        else
+        {
+            $alert = '<strong>Erreur d\'envoi de l\'e-mail</strong>';
+        }
+        
+        echo $alert;
+  }
  ?>
